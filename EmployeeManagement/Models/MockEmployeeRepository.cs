faefa0a22fd36465a29e724fe6687d9f9bd92462
Jose Thomas <jose8055@gmail.com>
2019-07-22 16:21:17 -0400

    AddedCoreDependencyInjection

---
+++ EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee() { Id = 1, Name = "Mary", Department = "HR", Email = "mary@wsdsds.com" },
+                new Employee() { Id = 1, Name = "Karen", Department = "IT", Email = "karen@wsdsds.com" },
+                new Employee() { Id = 1, Name = "Jan", Department = "HT", Email = "jan@wsdsds.com" }
+            };
+        }
+        public Employee GetEmployee(int Id)
+        {
+            return _employeeList.FirstOrDefault(e => e.Id == Id);
+        }
+    }
+}
