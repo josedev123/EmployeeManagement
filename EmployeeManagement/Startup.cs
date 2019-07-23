8d3099ab633148e2fa76ff37e0acf9785f6ddd85
Jose Thomas <jose8055@gmail.com>
2019-07-23 17:57:10 -0400

    AddTransient

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -17,7 +17,7 @@ namespace EmployeeManagement
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().AddXmlSerializerFormatters();
-            services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
+            services.AddTransient<IEmployeeRepository, MockEmployeeRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
