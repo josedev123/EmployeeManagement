ff548d9a11fefccf85a012e4080cbe0a3599eb56
Jose Thomas <jose8055@gmail.com>
2019-07-30 17:52:59 -0400

    Update Employee - Get & Post Actions

---
+++ EmployeeManagement/ViewModel/EmployeeEditViewModel.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.ViewModel
+{
+    public class EmployeeEditViewModel : EmployeeCreateViewModel
+    {
+        public int Id { get; set; }
+        public string ExistingPhotoPath { get; set; }
+    }
+}
