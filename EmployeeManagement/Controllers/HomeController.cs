0eabcd9142a9b329b27ba28f99b433b1da2a1796
Jose Thomas <jose8055@gmail.com>
2019-07-30 16:19:29 -0400

    Upload Multiple Files

--- EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using EmployeeManagement.Models;
 using EmployeeManagement.ViewModel;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -53,13 +54,16 @@ namespace EmployeeManagement.Controllers
             if (ModelState.IsValid)
             {
                         string uniqueFileName = null;
-                    if (model.Photo != null)
+                if (model.Photos != null && model.Photos.Count > 0)
+                {
+                    foreach (IFormFile photo in model.Photos)
                     {
-                       string uploadsFolder = Path.Combine(hostingEnvironment.WebRootPath, "images");
-                        uniqueFileName = Guid.NewGuid().ToString() + "_" + model.Photo.FileName;
-                       string filePath = Path.Combine(uploadsFolder, uniqueFileName);
-                        model.Photo.CopyTo(new FileStream(filePath, FileMode.Create));
+                        string uploadsFolder = Path.Combine(hostingEnvironment.WebRootPath, "images");
+                        uniqueFileName = Guid.NewGuid().ToString() + "_" + photo.FileName;
+                        string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                        photo.CopyTo(new FileStream(filePath, FileMode.Create));
                     }
+                }
 
                     Employee newEmployee = new Employee
                     {
