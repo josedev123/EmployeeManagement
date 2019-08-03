58dc7acba596aafdfee4b607d907f401d689706a
Jose <jose8055@gmail.com>
2019-08-03 10:25:11 -0400

    Custom Validation Attributr

--- EmployeeManagement/ViewModel/RegisterViewModel.cs
+++ EmployeeManagement/ViewModel/RegisterViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EmployeeManagement.Utilities;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -12,6 +13,7 @@ namespace EmployeeManagement.ViewModel
         [Required]
         [EmailAddress]
         [Remote(action: "IsEmailInUse", controller: "Account" )]
+        [ValidEmailDomain(allowedDomain: "imsadvertising.com", ErrorMessage = "Email domain must be imsadvertising.com")]
         public string Email { get; set; }
 
         [Required]
